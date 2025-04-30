d7d14ecd94a454b833e64065f2b3f94fb55007ac
Sergio Alfonso <arag_ong@yahoo.es>
2025-04-29 22:02:46 -0500

    feat: config sass variables colors and breakpoints

--- src/config/ui/font.ui.ts
+++ src/config/ui/font.ui.ts
@@ -3,5 +3,5 @@ import { Roboto_Condensed } from "next/font/google";
 export const robotoCondensed = Roboto_Condensed({
   subsets: ["latin"],
   weight: ["400", "500"],
-  variable: "--font-roboto-condensed",
+  variable: "--Font-family",
 });
