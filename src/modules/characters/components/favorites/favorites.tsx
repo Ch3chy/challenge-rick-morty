b68818ed10a72253222e55e419f3d1ebad75f574
Sergio Alfonso <arag_ong@yahoo.es>
2025-05-01 13:48:57 -0500

    feat: add list favorites component

---
+++ src/modules/characters/components/favorites/favorites.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { FC, useCallback, useRef, useState } from "react";
+import styles from "./favorites.module.scss";
+import { useSelector } from "react-redux";
+import { favoritesSelector } from "../../store/selectors";
+import { Trash } from "@phosphor-icons/react/dist/ssr";
+import { useAppDispatch } from "@/config/store";
+import { actions } from "../../store";
+import { Character } from "../../types/characters.types";
+
+type FavoritesProps = Readonly<{
+  className?: string;
+}>;
+
+const Favorites: FC<FavoritesProps> = ({ className }) => {
+  const favorites = useSelector(favoritesSelector);
+  const dispatch = useAppDispatch();
+
+  const listRef = useRef<HTMLDivElement>(null);
+  const [openList, setOpenList] = useState(false);
+
+  const handleRemoveFavorite = (character: Character) => {
+    dispatch(actions.removeFavorite(character));
+  };
+
+  const handleCloseOptions = useCallback((event?: Event) => {
+    if (!event || !listRef.current?.contains(event.target as Node)) {
+      setOpenList(false);
+      document.removeEventListener("mousedown", handleCloseOptions, false);
+    }
+  }, []);
+
+  const handleClick = () => {
+    setOpenList(!openList);
+    document.addEventListener("mousedown", handleCloseOptions, false);
+  };
+
+  return (
+    <div className={`${styles.favorites} ${className || ""}`} ref={listRef}>
+      {!openList && (
+        <button
+          className={`${styles.button} ${styles.item}`}
+          onClick={handleClick}
+        >
+          Favs
+        </button>
+      )}
+      {openList && (
+        <ul className={styles.list}>
+          {favorites.map((favorite) => (
+            <li className={styles.item} key={`favorite-${favorite.id}`}>
+              <span className={styles.name}>{favorite.name}</span>
+              <button
+                className={styles.remove}
+                onClick={() => handleRemoveFavorite(favorite)}
+              >
+                <Trash />
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Favorites;
--- src/modules/characters/layouts/mobile/mobile.tsx
+++ src/modules/characters/layouts/mobile/mobile.tsx
@@ -5,6 +5,7 @@ import { CarouselMobile } from "../../components/carousel-mobile";
 import { Character } from "../../types/characters.types";
 import { List } from "../../components/list";
 import { arrayToGroups } from "@/config/utils/arrays.utils";
+import { Favorites } from "../../components/favorites";
 
 type MobileLayoutProps = Readonly<{
   children?: React.ReactNode;
@@ -33,6 +34,9 @@ const MobileLayout: FC<MobileLayoutProps> = async ({
       <h1>Mobile Layout ({characters.length})</h1>
       <List characters={charactersGroups} className={styles.list} />
       <CarouselMobile characters={characters}>{children}</CarouselMobile>
+      <div className={styles.favorites}>
+        <Favorites />
+      </div>
     </div>
   );
 };
