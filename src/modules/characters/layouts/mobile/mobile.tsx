c59d752e307683d85aa093552a86a1d73f99b83b
Sergio Alfonso <arag_ong@yahoo.es>
2025-05-01 01:14:42 -0500

    feat: add title image to mobile layout

--- src/modules/characters/layouts/mobile/mobile.tsx
+++ src/modules/characters/layouts/mobile/mobile.tsx
@@ -1,15 +1,30 @@
 import { FC } from "react";
 import styles from "./mobile.module.scss";
 import { Character } from "rickmortyapi";
+import Image from "next/image";
 
 type MobileLayoutProps = Readonly<{
   children?: React.ReactNode;
   characters: Character[];
 }>;
 
-const MobileLayout: FC<MobileLayoutProps> = async ({ children, characters }) => {
+const MobileLayout: FC<MobileLayoutProps> = async ({
+  children,
+  characters,
+}) => {
   return (
     <div className={styles.layout}>
+      <header className={styles.header}>
+        <h1 className={styles.title}>
+          Rick & Morty
+          <Image
+            src="/assets/images/title.webp"
+            alt="Rick & Morty"
+            width={100}
+            height={100}
+          />
+        </h1>
+      </header>
       <h1>Mobile Layout ({characters.length})</h1>
       {children}
     </div>
